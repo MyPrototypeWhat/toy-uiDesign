7d9c8bc580c56c3398a767ad8a9363747cb545c1
lizhixuan <1398437825@qq.com>
2019-04-11 12:52:06 +0800

    addLessLoader

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
-
+import { Toast } from './components/Toast';
 class App extends Component {
   render() {
     return (
-      null
+      <Toast/>   
     );
   }
 }
---
+++ src/components/Toast/index.js
@@ -0,0 +1,2 @@
+import {Toast} from './view/toast';
+export {Toast};

1dc1f206158250228fe6069ad49057aa6dcbdee6
lizhixuan <1398437825@qq.com>
2019-04-12 14:43:49 +0800

    addScrollBar14.43

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import { ScrollBar } from './components/ScrollBar';
 class App extends Component {
   render() {
     return (
-      <ScrollBar style={{height:'400px'}}>
-        <div style={{height:'2000px'}}>
+      <ScrollBar style={{height:'400px',width:'200px'}} scrollY={true} scrollX={true}>
+        {/* 只能用唯一子元素 */}
+        <div style={{height:'5000px',width:'1000px'}}>
           123123123123123123
         </div>
       </ScrollBar>
--- src/components/ScrollBar/view/scrollBar.js
+++ src/components/ScrollBar/view/scrollBar.js
@@ -4,41 +4,93 @@ export class ScrollBar extends React.Component{
     state={
         scrollWidth:0,
         scrollYHeight:0,
+        scrollXWidth:0,
+        translateY:0,
+        translateX:0
+    }
+    scrollYHeight=(scrollBar,container)=>{
+        const containerHeight=container.clientHeight;
+        const scrollBarHeight=scrollBar.children[0].clientHeight;
+        var scrollYHeight=this.scrollOpt(scrollBarHeight,containerHeight);
+        this.setState({
+            scrollYHeight
+        });
+    }
+    scrollXWidth(scrollBar,container){
+        const containerWidth=container.clientWidth;
+        const scrollBarWidth=scrollBar.children[0].clientWidth;
+        var scrollXWidth=this.scrollOpt(scrollBarWidth,containerWidth);
+        this.setState({
+            scrollXWidth
+        });
+    }
+    scrollOpt=(viewOpt,containerOpt)=>{
+        var result=containerOpt*containerOpt/(viewOpt-containerOpt);
+        return result
+    }
+    scrollWidth=(scrollBar)=>{
+        var scrollWidth=scrollBar.offsetWidth-scrollBar.clientWidth;
+        this.setState({
+            scrollWidth,
+        });
+    }
+    onScroll=(scrollBar,container)=>{
+        scrollBar.onscroll=(x)=>{
+            this.onScrollY(scrollBar,container);
+            this.onScrollX(scrollBar,container);
+        }
+    }
+    onScrollX=(scrollBar,container)=>{
+        const containerWidth=container.clientWidth;
+        const scrollBarWidth=scrollBar.children[0].clientWidth
+        var scrollYWidth=containerWidth*containerWidth/(scrollBarWidth-containerWidth);
+        let translateX=(containerWidth-scrollYWidth)/(scrollBarWidth-containerWidth)*scrollBar.scrollLeft;
+        this.setState({
+            translateX
+        })
+    }
+    onScrollY=(scrollBar,container)=>{
+        const containerHeight=container.clientHeight;
+        const scrollBarHeight=scrollBar.children[0].clientHeight
+        var scrollYHeight=containerHeight*containerHeight/(scrollBar.children[0].clientHeight-containerHeight);
+        let translateY=(containerHeight-scrollYHeight)/(scrollBarHeight-containerHeight)*scrollBar.scrollTop;
+        this.setState({
+            translateY
+        })
     }
     componentDidMount(){
+        const container=document.querySelector('.Chaos-scrollBar');
         const scrollBar=document.querySelector('.Chaos-scrollBar-main');
-        var scrollWidth=scrollBar.offsetWidth-scrollBar.clientWidth;
-        console.log(scrollWidth);
-        this.setState({
-            scrollWidth
-        });
-        scrollBar.onscroll=(x)=>{
-            // console.log(1);
-            // console.log(scrollBar.scrollTop);
-        }
-        document.querySelector('.Chaos-scrollBar').onmouseover=()=>{
-            console.log(123);
-        }
-        // console.log();
+        this.scrollWidth(scrollBar);
+        this.scrollXWidth(scrollBar,container);
+        this.scrollYHeight(scrollBar,container);
+        this.onScroll(scrollBar,container);
+        
+        // containerHeight.onmouseover=()=>{
+            
+        // }
     }
     render(){
        return (
            <div className='Chaos-scrollBar' style={this.props.style} >
                <div className='Chaos-scrollBar-main'style={{marginRight:-this.state.scrollWidth,marginBottom:-this.state.scrollWidth}}>
                     {
-                        this.props.scrollY
-                        ?(
-                            <div className='Chaos-scrollBar-Y' style={{width:this.state.scrollWidth,...this.props.scrollBarYStyle}}></div>
-                        ):null
-                    }
-                    {
                         this.props.children
                     }
                </div>
                     {
+                        this.props.scrollY
+                        ?(
+                            <div className='Chaos-scrollBar-Y' 
+                            style={{height:this.state.scrollYHeight+'px',width:'6px',transform:`translateY(${this.state.translateY}px)`}}></div>
+                        ):null
+                    }
+                    {
                         this.props.scrollX
                         ?(
-                            <div className='Chaos-scrollBar-X'></div>
+                            <div className='Chaos-scrollBar-X'
+                            style={{height:'6px',width:this.state.scrollXWidth+'px',transform:`translateX(${this.state.translateX}px)`}}
+                            ></div>
                         ):null
                     }
            </div>
